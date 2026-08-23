2c7c4d10d7a89d6030e6d373a07859fed14a08dd
Rana Sherif <eng.rana.sherif@gmail.com>
2026-08-24 01:03:33 +0300

    Update motor control implementation for L298N, Cytron MD10A, and BTS7960

---
+++ CytronMD10A.h
@@ -0,0 +1,12 @@
+#ifndef CYTRONMD10A_H_
+#define CYTRONMD10A_H_
+#include<Arduino.h>
+
+void Cytron_init(int DIR, int PWM);
+void Cytron_drive(int DIR,int PWM,int speed_in_pwm)
+void Forward();
+void Backward();
+void Stop();
+
+
+#endif
---
+++ L298N.h
@@ -0,0 +1,13 @@
+#ifndef L298N_H_
+#define L298N_H_
+#include<Arduino.h>
+
+void L298N_init(int IN1, int IN2, int ENA);
+void L298N_drive(int IN1, int IN2, int ENA, int speed_in_pwm);
+
+void Forward();
+void Backward();
+void Stop();
+
+
+#endif
